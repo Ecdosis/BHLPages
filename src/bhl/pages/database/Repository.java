0a2bead74182aebe5d4fb819e9c61aa4c36510eb
desmond <desmond.allan.schmidt@gmail.com>
2015-03-13 10:27:15 +1000

    Updated copyright notices.

--- src/bhl/pages/database/Repository.java
+++ src/bhl/pages/database/Repository.java
@@ -1,18 +1,22 @@
-/* This file is part of MML.
+/*
+ * This file is part of BHLPages.
  *
- *  MML is free software: you can redistribute it and/or modify
+ *  BHLPages is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  MML is distributed in the hope that it will be useful,
+ *  BHLPages is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with MML.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with BHLPages.  If not, see <http://www.gnu.org/licenses/>.
+ *  (c) copyright Biodiversity Heritage Library 2015 
+ *  http://www.biodiversitylibrary.org/
  */
+
 package bhl.pages.database;
 
 /**
